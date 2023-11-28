dc340728ab4eaf5b7aa984a7dc236ff7b6efafe1
Suzana <su_zana-santos@hotmail.com>
2023-11-28 09:32:44 -0300

    Adicionando arquivos

---
+++ public/array.php
@@ -0,0 +1,12 @@
+<?php
+
+$frutas = array("morango", "abacaxi", "pera");
+$verduras = array("agriao", "alho-poro", "brocolis");
+
+echo $frutas[1].PHP_EOL;
+echo $verduras[2].PHP_EOL;;
+
+$nascimento = new DateTime();
+var_dump($nascimento);
+
+//para verificar se uma variavel ja foi definida, usa-se o comando isset e o comando unset para eliminar a variavel
